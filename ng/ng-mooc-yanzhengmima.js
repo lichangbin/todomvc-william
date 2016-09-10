b9826890bb7b626dc17e35041f4117fa251104b7
william-xue <1083077511@qq.com>
2016-09-10 15:12:48 +0800

    ljia

---
+++ ng/ng-mooc-yanzhengmima.js
@@ -0,0 +1,37 @@
+/* 
+* @Author: anchen
+* @Date:   2016-09-07 10:47:16
+* @Last Modified by:   anchen
+* @Last Modified time: 2016-09-10 14:07:53
+*/
+ angular.module('myapp',[])
+    .controller("main",
+        function($scope) {
+                    $scope.userdata={};
+
+                    $scope.sub=function(){
+                        alert($scope.userdata.username)
+                        if ($scope.signup.$invalid) 
+                            alert("false");
+                        else alert("输入正确")
+                    }
+                      }
+                 )
+    /*.directive('compare',function(){
+                        var o={};
+                        o.strict='AE';
+                        o.scope={
+                            orgText:'=compare'
+                        };
+                        o.require='ngModle';
+                        o.link=function(sco.ele.attr.con){
+                                con.$validators.compare=function(v){
+                                    return v==sco.orgText;
+                                }
+
+                                sco.$watch('orgText',function(){
+                                    con.$validate()})
+
+                        }
+    })
+*/
